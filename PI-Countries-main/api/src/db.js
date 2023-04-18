068f5cf6d182279e49280ffba570bc832e9c90fe
Alejandra Blanco <davidrey20@outlook.es>
2023-04-17 19:15:46 -0500

    variables udated

--- PI-Countries-main/api/src/db.js
+++ PI-Countries-main/api/src/db.js
@@ -6,7 +6,7 @@ const {
   PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`, {
+const sequelize = new Sequelize(`postgres://postgres:pWyUsNurJ5CmxIjSIQt0@containers-us-west-143.railway.app:6974/railway`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
